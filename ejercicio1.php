ca4ddeb0a39017383cac8927c3bd02e7b9e28f25
MoonDarkKj17 <73483652+MoonDarkKj17@users.noreply.github.com>
2020-11-23 09:48:55 -0500

    Create ejercicio1.php

---
+++ ejercicio1.php
@@ -0,0 +1,16 @@
+<? php	
+$color = array(4 => 'blanco', 6 => 'verde', 11=> 'roja');
+var_dump($color);
+$primerColor = $primerColor[4];
+echo "El resultado esperado es: $primerColor";
+
+
+
+
+
+
+
+
+
+
+?>
